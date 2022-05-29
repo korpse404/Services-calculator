67d130f67eb7eaebf4d9133ed6e98eab0384e965
Pavel <vkorpv@gmail.com>
2022-05-29 19:01:11 +0300

    Second lesson, homework completed and ready for checking

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,18 @@
-const title = '';
-const screens = 2;
-const screenPrice = 3;
-const rollback = true;
-const fullPrice = 1;
+const title = "Учебный проект 1";
+const screens = "Простые, Сложные, Интерактивные";
+const screenPrice = 100;
+const rollback = 5;
+const fullPrice = 1500;
 const adaptive = true;
-
 
 alert("Это - всплывающее сообщение");
 console.log("А это - сообщение в консоли");
+
+console.log("title - это " + typeof title + ",", " fullPrice - это " + typeof fullPrice + ",", " adaptive - это " + typeof adaptive);
+console.log(`Длинна строки ${screens.length}`);
+console.log(`Стоимость верстки экранов ${screenPrice} долларов`);
+console.log(`Стоимость разработки сайта ${fullPrice} долларов`);
+
+screens.toLowerCase();
+console.log(screens.split(", "));
+console.log(`Процент отката посреднику за работу ${(fullPrice * (rollback/100))}`)
